efdfe0f99604e19206213419e8b3455ef8f9559e
Anisa Majmari <anisamajmari@gmail.com>
2023-12-04 17:33:01 +0100

    home page, recent offers section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "../components/Slider";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import { Link } from "react-router-dom";
+
+import ListingItem from "../components/ListingItem";
 
 export default function Home() {
+  // Offers
+  const [offerListings, setOfferListings] = useState(null);
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        // get reference
+        const listingsRef = collection(db, "listings");
+
+        // create query
+        const q = query(
+          listingsRef,
+          where("offer", "==", true),
+          orderBy("timestamp", "desc"),
+          limit(4)
+        );
+
+        // execute the query
+        const querySnap = await getDocs(q);
+        const listings = [];
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setOfferListings(listings);
+      } catch (error) {}
+    };
+
+    fetchListings();
+  }, []);
+
   return (
     <div>
       <Slider />
+      <div className="max-w-6xl mx-auto pt-4 space-y-6">
+        {offerListings && offerListings.length > 0 && (
+          <div className="m-2 mb-6">
+            <h2 className="px-3 text-2xl mt-6 font-semibold">Recent offers</h2>
+            <Link to="/offers">
+              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
+                Show more offers
+              </p>
+            </Link>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {offerListings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                />
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
